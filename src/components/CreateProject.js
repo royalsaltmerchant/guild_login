194d0ddfcc77303c1b25e4da77115831addb67e2
Julian ranieri <jumpingafterrain@gmail.com>
2021-06-21 10:54:56 -0700

    working on render entries for create project

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -1,3 +1,4 @@
+import { entries } from 'mobx'
 import React, { Component } from 'react'
 import {Spinner, Button, Form} from 'react-bootstrap'
 
@@ -5,13 +6,50 @@ export default class CreateProject extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      entries: [1],
     }
   }
 
+  renderEntries() {
+    const {entries} = this.state
+    const elements = []
+    entries.forEach(entry => {
+      elements.push(
+        <div className="d-flex justify-content-end">
+          <Form.Group controlId={`asset${entry}`}>
+            <Form.Label>{`asset ${entry}`}</Form.Label>
+            <Form.Control 
+              required
+              size="md"
+              type="text"
+              placeholder="Project Asset" />
+          </Form.Group>
+          <Button variant="link" style={{color: 'red', fontSize: '30px'}} onClick={() => this.removeAsset(entry)}>
+          ×
+          </Button>
+        </div>
+      )
+    })
+    return elements
+  }
+
+  removeAsset(entry) {
+    const {entries} = this.state
+    const filteredEntries = entries.filter(oldEntry => {
+      return oldEntry !== entry
+    })
+    this.setState({entries: [...filteredEntries]})
+  }
+
+  handleSubmit(event) {
+    console.log(event.target)
+  }
+
   render() {
+    console.log(this.state.entries)
     return (
       <div className="p-3">
+        
         <Form onSubmit={(event) => this.handleSubmit(event)}>
           <Form.Group controlId="name">
             <Form.Label>Name</Form.Label>
@@ -20,6 +58,15 @@ export default class CreateProject extends Component {
               size="md"
               type="text"
               placeholder="Project Name" />
+          </Form.Group>
+
+          <Form.Group controlId="image">
+            <Form.Label>Image</Form.Label>
+            <Form.Control 
+              size="md" 
+              type="file"
+              accept="image/*" 
+             />
           </Form.Group>
 
           <Form.Group controlId="description">
@@ -32,17 +79,17 @@ export default class CreateProject extends Component {
               placeholder="Project Description" />
           </Form.Group>
 
-          <Form.Group controlId="image">
-            <Form.Label>Image</Form.Label>
-            <Form.Control 
-              size="md" 
-              type="file"
-              accept="image/*" 
-             />
-          </Form.Group>
-            <Button variant="outline-success" type="submit">
-              Create
-            </Button>
+          {this.renderEntries()}
+
+          <Button variant="link" onClick={() => this.setState({entries: [...this.state.entries, this.state.entries[this.state.entries.length - 1] + 1]})}>
+            + Asset
+          </Button>
+
+          <hr />
+
+          <Button variant="outline-success" type="submit">
+            Create
+          </Button>
         </Form>
       </div>
     )
