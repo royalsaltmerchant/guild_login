ce3da65771577edaa1456fb89688060c5d7f33cc
Julian ranieri <jumpingafterrain@gmail.com>
2021-06-24 14:58:45 -0700

    video and styling on pack details

--- src/components/PackDetails.js
+++ src/components/PackDetails.js
@@ -19,7 +19,7 @@ class PackDetails extends Component {
       this.setState({
         packDetails: {
           title: 'Ancient Weapons Pack',
-          Description: 'its got wonderful amazing sounds',
+          Description: 'Combat sound effects for games set in medieval or fantasy settings. Complete with swings impacts and custom assembled attacks and combos for Swords, Daggers, Axes and more.',
         },
         assetTypes: ['one', 'two', 'three', 'four']
       })
@@ -48,10 +48,10 @@ class PackDetails extends Component {
     const {packDetails, assetTypes} = this.state
     if(packDetails !== {} && assetTypes.length !== 0) {
       return(
-        <div>
+        <div className="w-50">
           <h1>{packDetails.title}</h1>
           <div className="px-3">
-            <p>Description: "{packDetails.Description}"</p>
+            <p>"{packDetails.Description}"</p>
             <p>Asset Types:</p>
             {this.renderAssetTypes()}
           </div>
@@ -64,24 +64,32 @@ class PackDetails extends Component {
     }
   }
 
-  renderPackImage() {
+  renderPackImageAndVideo() {
     const {packName} = this.props.match.params
     if(packName === 'ancient-weapons-pack') {
       return(
-        <Image className="pack-img mr-3" src={`${config.image_URL}weaponpackcolor.jpg`} />
+        <div className="d-flex flex-column">
+          <Image className="pack-img mr-3 mb-5" src={`${config.image_URL}weaponpackcolor.jpg`} />
+          <h3 className="text-center">Audio Demo</h3>
+          <iframe class="video" src="https://www.youtube.com/embed/gKJfZtoXlkg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       )
     }
     if(packName === 'ancient-magic-pack') {
       return(
-        <Image className="pack-img mr-3" src={`${config.image_URL}magicpackcolor.jpg`} />
+        <div className="d-flex flex-column">
+          <Image className="pack-img mr-3 mb-5" src={`${config.image_URL}magicpackcolor.jpg`} />
+          <h3 className="text-center">Audio Demo</h3>
+          <iframe class="video" src="https://www.youtube.com/embed/dBPSmhcLOBA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       )
     }
   }
 
   render() {
     return (
-      <div className="d-flex flex-row flex-wrap">
-        {this.renderPackImage()}
+      <div className="d-flex flex-row flex-wrap justify-content-center w-75 p-3 border border-light rounded" style={{backgroundColor: '#fff'}}>
+        {this.renderPackImageAndVideo()}
         {this.renderTrackDetails()}
       </div>
     )
