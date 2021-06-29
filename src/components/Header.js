12164915db00bd047b94c76a3aaa7b0cc7efc1d4
Julian ranieri <jumpingafterrain@gmail.com>
2021-06-29 12:13:45 -0700

    header image as link

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,13 @@ export default class Header extends Component {
     return (
       <div className="d-flex flex-column justify-content-center align-items-center">
         <Media>
+          <a href="https://sfaudioguild.com">
           <img
             width={100}
             height={100}
             src={sfag}
           />
+          </a>
         </Media>
         <hr style={{width: '75%'}}/>
       </div>
