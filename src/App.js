f90c9588d03e76bd71571bf513f3bcf6c25a7e1f
mda100 <mdadams10@gmail.com>
2021-08-09 23:51:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 import {Provider as MobxStoreProvider} from 'mobx-react';
 import mobxStoresToInject from './mobx'
@@ -19,37 +19,57 @@ import Dashboard from './components/Dashboard';
 import Upload from './components/Upload'
 import Library from './components/Library';
 import PackDetails from './components/PackDetails';
+import AccessDenied from './components/AccessDenied';
+import NoMatch from './components/NoMatch';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap'
+import {authenticate as authenticateAPICall} from './config/api'
+
 
 const mobxStores = {
   ...mobxStoresToInject
 }
 
-function Root() {
-  return(
-    <Container fluid className="App">
+class Root extends React.Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      authorized: false
+    }
+  }
+
+  async componentDidMount() {
+    const token = localStorage.getItem('token')
+    if(token) {
+      try {
+        const res = await authenticateAPICall()
+        if(res.status === 200) {
+          this.setState({
+            authorized: true
+          })
+        }
+      } catch(err) {
+        console.log(err)
+      }
+    } else {
+      this.setState({
+        authorized: false
+      })
+    }
+  } 
+
+  render(){
+    return <Container fluid className="App">
       <Header />
-      <NavBar />
+      <NavBar authorized={this.state.authorized} />
       <div className="d-flex justify-content-center p-3 rounded border border-light" style={{backgroundColor: '#f6f6f6'}}>
         <Switch>
-        <Route exact path="/">
+          <Route exact path="/">
             <Redirect to="/login" />
-          </Route>
-        <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/login">
-            <Login />
           </Route>
           <Route path="/logout">
             <Logout />
-          </Route>
-          <Route path="/account">
-            <Account />
-          </Route>
-          <Route path="/Dashboard">
-            <Dashboard />
           </Route>
           <Route path="/library">
             <Library />
@@ -57,13 +77,26 @@ function Root() {
           <Route path="/pack/:packName">
             <PackDetails />
           </Route>
-          <Route path="/Upload/entry/:entryId">
-            <Upload />
+          <Route path="/register">
+            <Register />
           </Route>
+          <Route path="/login">
+            {!this.state.authorized? <Login /> : <div><h2 className="text-center">Login</h2><p> already logged in</p> </div>}
+          </Route>
+          <Route path="/account">
+            {this.state.authorized? <Account /> : <AccessDenied />}
+          </Route>
+          <Route path="/Dashboard">
+            {this.state.authorized? <Dashboard /> : <AccessDenied />}
+          </Route>
+          <Route path="/Upload/entry/:entryId">
+            {this.state.authorized? <Upload /> : <AccessDenied />}
+          </Route>
+          <Route render={() => <NoMatch />}/>
         </Switch>
       </div>
     </Container>
-  )
+  }
 }
 
 function App() {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,44 +2,17 @@ import React, { Component } from 'react'
 import {
   Link
 } from "react-router-dom";
-import {authenticate as authenticateAPICall} from '../config/api'
 
 export default class NavBar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      authorized: false
-    }
-  }
-
-  async componentDidMount() {
-    const token = localStorage.getItem('token')
-    if(token) {
-      try {
-        const res = await authenticateAPICall()
-        if(res.status === 200) {
-          this.setState({
-            authorized: true
-          })
-        }
-      } catch(err) {
-        console.log(err)
-      }
-    } else {
-      this.setState({
-        authorized: false
-      })
-    }
-  } 
 
   render() {
     return(
       <div className="d-flex flex-column align-items-center justify-content-center">
         <div className="w-75 d-flex justify-content-around">
-          {this.state.authorized ? <Link to="/account">Account</Link> : null}
-          {this.state.authorized ? <Link to="/dashboard">Dashboard</Link> : null}
+          {this.props.authorized && <Link to="/account">Account</Link>}
+          {this.props.authorized && <Link to="/dashboard">Dashboard</Link>}
           <Link to="/library">Library</Link>
-          {this.state.authorized ? <Link to="/logout">Logout</Link> : <Link to="/login">Login</Link>}
+          {this.props.authorized ? <Link to="/logout">Logout</Link> : <Link to="/login">Login</Link>}
         </div>
         <hr style={{width: '75%'}}/>
       </div>
