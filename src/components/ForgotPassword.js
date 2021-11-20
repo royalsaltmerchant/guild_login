785cfbea06f7fedbf761d15624ec288f57d7b707
Julian ranieri <jumpingafterrain@gmail.com>
2021-11-19 19:43:54 -0800

    api call for reset password

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,22 +1,69 @@
 import React, { Component } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Alert } from 'react-bootstrap'
+import { requestResetEmail as requestResetEmailAPICall } from '../config/api'
 
 export default class ForgotPassword extends Component {
   constructor() {
     super()
     this.state = {
-      
+      alert: false,
+      alertType: 'warning',
+      alertText: 'Something went wrong, please try again.'
     }
   }
 
-  handleSubmit(event) {
+  componentDidUpdate() {
+    if(this.state.alert) {
+      setTimeout(() => {
+        this.setState({
+          alert: false
+        })
+      }, 10000)
+    }
+  }
+
+  renderAlert() {
+    if(this.state.alert) {
+      return(
+        <Alert variant={this.state.alertType}>
+          {this.state.alertText}
+        </Alert>
+      )
+    }
+  }
+
+  async handleSubmit(event) {
     event.preventDefault()
     const email = event.target.email.value.trim()
-    console.log(email)
+    try {
+      const res = await requestResetEmailAPICall(email)
+      if(res.status === 200) {
+        this.setState({
+          alert: true,
+          alertText: "Sent! Please check your email for instruction.",
+          alertType: "success"
+        })
+      }
+    } catch(err) {
+      if(err.response && err.response.status === 400) {
+        this.setState({
+          alert: true,
+          alertText: "No user found with this email.",
+          alertType: "warning"
+        })
+      } else {
+        this.setState({
+          alert: true,
+          alertText: "Something went wrong, please try again.",
+          alertType: "danger"
+        })
+      }
+    }
   }
   render() {
     return (
       <div className="w-50">
+        {this.renderAlert()}
         <h2 className="pb-3 text-center">Forgot Password</h2>
         <Form onSubmit={(event) => this.handleSubmit(event)}>
           <Form.Group controlId="email">
--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,11 +1,74 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";  
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+import { resetPassword as resetPasswordAPICall } from '../config/api';
 
-export default class ResetPassword extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+export default function ResetPassword() {
+  const [alert, setAlert] = useState(false)
+  const [alertText, setAlertText] = useState('Something went wrong, please try again later!')
+  const [alertType, setAlertType] = useState('warning')
+  const {token} = useParams()
+
+  useEffect(() => {
+      if(alert) {
+        setTimeout(() => {
+          setAlert(false)
+        }, 10000)
+      }
+  });
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const password = event.target.password.value
+
+    try {
+      const res = await resetPasswordAPICall(password, token)
+      if(res.status === 200) {
+        setAlert(true)
+        setAlertType('success')
+        setAlertText('Password Has Been Reset!')
+      } else {
+        setAlert(true)
+        setAlertType('warning')
+        setAlertText('Your request has expired or is invalid.')
+      }
+    } catch (error) {
+      setAlert(true)
+      setAlertType('danger')
+      setAlertText('Something Went Wrong, Please Try Again Later.')
+    }
+    
   }
+
+  function renderAlert() {
+    if(alert) {
+      return(
+        <Alert variant={alertType}>
+          {alertText}
+        </Alert>
+      )
+    }
+  }
+
+  return (
+    <div className="w-50">
+      {renderAlert()}
+      <h2 className="pb-3 text-center">Reset Password</h2>
+      <Form onSubmit={(event) => handleSubmit(event)}>
+      <Form.Group controlId="password">
+          <Form.Label>Enter your new password</Form.Label>
+          <Form.Control 
+            required
+            size="md"
+            type="text"
+            placeholder="New Password" />
+        </Form.Group>
+        <Button variant="outline-success" type="submit">
+          Reset
+        </Button>
+      </Form>
+    </div>
+  )
 }
