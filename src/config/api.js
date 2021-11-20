7a01b84db506f1a1e9da509fccd040f6d97cb684
Julian ranieri <jumpingafterrain@gmail.com>
2021-11-20 10:51:27 -0800

    cleanup api.js

--- src/config/api.js
+++ src/config/api.js
@@ -2,482 +2,362 @@ import {finalConfig as config} from './config'
 import axios from 'axios'
 
 const login = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/login`,
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/login`,
+    data: params
+  })
+  return res
 }
 
 const authenticate = async () => {
-  try {
-    const res = await axios({
-      method: 'get',
-      url: `${config.apiURL}/api/verify_jwt`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'get',
+    url: `${config.apiURL}/api/verify_jwt`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    }
+  })
+  return res
 }
 
 const getUser = async () => {
-  try {
-    const res = await axios({
-      method: 'get',
-      url: `${config.apiURL}/api/get_user`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'get',
+    url: `${config.apiURL}/api/get_user`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    }
+  })
+  return res
 }
 
 const getUsers = async () => {
-  try {
-    const res = await axios({
-      method: 'get',
-      url: `${config.apiURL}/api/users`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'get',
+    url: `${config.apiURL}/api/users`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    }
+  })
+  return res
 }
 
 const registerUser= async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/register`,
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/register`,
+    data: params
+  })
+  return res
 }
 
 const editUser= async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/edit_user`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/edit_user`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const getProjects = async () => {
-  try {
-    const res = await axios({
-      method: 'get',
-      url: `${config.apiURL}/api/projects`,
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'get',
+    url: `${config.apiURL}/api/projects`,
+  })
+  return res
 }
 
 const getProject = async (projectId) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/get_project`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        project_id: projectId
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/get_project`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      project_id: projectId
+    }
+  })
+  return res
 }
 
 const createProject = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/add_project`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/add_project`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const editProject = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/edit_project`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/edit_project`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const deleteProject = async (projectId) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/remove_project`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        project_id: projectId,
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/remove_project`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      project_id: projectId,
+    }
+  })
+  return res
 }
 
 const getEntry = async (entryId) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/get_entry`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        entry_id: entryId
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/get_entry`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      entry_id: entryId
+    }
+  })
+  return res
 }
 
 const createEntry = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/add_entry`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/add_entry`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const editEntry = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/edit_entry`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/edit_entry`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const deleteEntry = async (entryId) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/remove_entry`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        entry_id: entryId,
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/remove_entry`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      entry_id: entryId,
+    }
+  })
+  return res
 }
 
 const createContribution = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/add_contribution`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/add_contribution`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const editContribution = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/edit_contribution`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/edit_contribution`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const deleteContribution = async (contributionId) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/remove_contribution`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        contribution_id: contributionId,
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/remove_contribution`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      contribution_id: contributionId,
+    }
+  })
+  return res
 }
 
 const createContributedAsset = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/add_contributed_asset`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/add_contributed_asset`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const getPresignedURL = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/signed_URL`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/signed_URL`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const getPacks = async () => {
-  try {
-    const res = await axios({
-      method: 'get',
-      url: `${config.apiURL}/api/packs`,
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'get',
+    url: `${config.apiURL}/api/packs`,
+  })
+  return res
 }
 
 const getPack = async (packTitle) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/get_pack_by_title`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        pack_title: packTitle
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/get_pack_by_title`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      pack_title: packTitle
+    }
+  })
+  return res
 }
 
 const createPack = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/add_pack`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/add_pack`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const editPack = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/edit_pack`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/edit_pack`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const deletePack = async (packId) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/remove_pack`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        pack_id: packId,
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/remove_pack`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      pack_id: packId,
+    }
+  })
+  return res
 }
 
 const createAssetType = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/add_asset_type`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/add_asset_type`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const editAssetType = async (params) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/edit_asset_type`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: params
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/edit_asset_type`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: params
+  })
+  return res
 }
 
 const deleteAssetType = async (assetTypeId) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/remove_asset_type`,
-      headers: {
-        "x-access-token": localStorage.getItem("token")
-      },
-      data: {
-        asset_type_id: assetTypeId,
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/remove_asset_type`,
+    headers: {
+      "x-access-token": localStorage.getItem("token")
+    },
+    data: {
+      asset_type_id: assetTypeId,
+    }
+  })
+  return res
 }
 
 const requestResetEmail = async (email) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/request_reset_email`,
-      data: {
-        email: email
-      }
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/request_reset_email`,
+    data: {
+      email: email
+    }
+  })
+  return res
 }
 
 const resetPassword = async (password, token) => {
-  try {
-    const res = await axios({
-      method: 'post',
-      url: `${config.apiURL}/api/reset_password`,
-      data: {
-        password: password,
-        token: token
-      },
-      validateStatus: (status) => status === 200 || status === 400
-    })
-    return res
-  } catch(err) {
-    throw(err)
-  }
+  const res = await axios({
+    method: 'post',
+    url: `${config.apiURL}/api/reset_password`,
+    data: {
+      password: password,
+      token: token
+    },
+    validateStatus: (status) => status === 200 || status === 400
+  })
+  return res
 }
 
 export {
