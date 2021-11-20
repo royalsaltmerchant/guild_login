ecf5b1a67cea4da465abb8bf079828ce901de36e
Julian ranieri <jumpingafterrain@gmail.com>
2021-11-19 18:34:13 -0800

    route for reset

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap'
 import {authenticate as authenticateAPICall} from './config/api'
 import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
 
 
 const mobxStores = {
@@ -111,6 +112,9 @@ class Root extends React.Component{
           </Route>
           <Route path="/forgot_password">
             <ForgotPassword />
+          </Route>
+          <Route path="/reset_password/:token">
+            <ResetPassword />
           </Route>
           <Route render={() => <NoMatch />}/>
         </Switch>
