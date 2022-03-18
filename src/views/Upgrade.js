f5b993b0c13846de6a039c12911529d620c88012
Julian ranieri <jumpingafterrain@gmail.com>
2022-03-17 23:39:52 -0700

    premium message to email

--- src/views/Upgrade.js
+++ src/views/Upgrade.js
@@ -4,6 +4,7 @@ export default function Upgrade() {
   return(
     <div>
       <h3>Upgrade to Premium</h3>
+      <p>Under Construction. Please email <a href='mailto:admin@sfaudioguild.com'>admin@sfaudioguild.com</a> for premium.</p>
     </div>
   )
 }
